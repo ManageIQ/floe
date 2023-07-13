b05a2bc5a39d231e28843af5adb6287a6a28f587
Adam Grare <adam@grare.com>
2023-07-13 11:16:35 -0400

    Success state doesn't have input_path/output_path

--- lib/floe/workflow/states/succeed.rb
+++ lib/floe/workflow/states/succeed.rb
@@ -4,15 +4,6 @@ module Floe
   class Workflow
     module States
       class Succeed < Floe::Workflow::State
-        attr_reader :input_path, :output_path
-
-        def initialize(workflow, name, payload)
-          super
-
-          @input_path  = Path.new(payload.fetch("InputPath", "$"))
-          @output_path = Path.new(payload.fetch("OutputPath", "$"))
-        end
-
         def end?
           true # TODO: Handle if this is ending a parallel or map state
         end
@@ -20,8 +11,7 @@ module Floe
         private
 
         def execute!(input)
-          input = input_path.value(context, input)
-          output_path&.value(context, input)
+          input
         end
       end
     end
